7d5185dd744acc0ceeac8045814e1d1e4218cc0d
EgorVeremeychik <Veremeychikegor95@mail.ru>
2016-02-03 21:57:24 +0300

    Create AirlineServlet

---
+++ AirlineWebProject/src/main/java/by/bsuir/airline/servlet/AirlineServlet.java
@@ -0,0 +1,27 @@
+package by.bsuir.airline.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by EgorVeremeychik on 03.02.2016.
+ */
+public class AirlineServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
